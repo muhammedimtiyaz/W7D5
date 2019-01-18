4c63e567bd36f9f1a22ef13cc8da44f6d9910b9d
Muhammed <muhammedimtiyaz87@gmail.com>
2019-01-18 17:36:01 -0500

    Finish session reducer

--- BenchBnb/frontend/benchbnb.jsx
+++ BenchBnb/frontend/benchbnb.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as UserApiUtils from "./utils/users_api_utils";
+
 
 document.addEventListener("DOMContentLoaded", () => {
-  window.signup = UserApiUtils.signup;
-  window.login = UserApiUtils.login;
-  window.logout = UserApiUtils.logout;
+  // window.signup = UserApiUtils.signup;
+  // window.login = UserApiUtils.login;
+  // window.logout = UserApiUtils.logout;
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Hello</h1>, root)
 });
